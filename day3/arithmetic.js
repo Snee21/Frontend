6e0be431cee08394b1cc7e9c736e11dda80ee982
Snee21 <snekasiva04@gmail.com>
2024-06-22 17:47:07 +0530

    1st commit

---
+++ day3/arithmetic.js
@@ -0,0 +1,13 @@
+let car={
+    carname:"honda",
+    carprice:20000,
+    carcolor:"red"
+}
+for(x in c)
+    document.write(c[v]);
+
+let lms="she is a girl"
+let n=lms.slice(1,3)
+console.log(n)
+console.log(lms.substring(2,5))
+console.log(lms.substr(1,3))
---
+++ day4/pen.js
@@ -0,0 +1,20 @@
+const pen={
+    name:"reynolds",
+    price:21,
+    color:"black"
+}
+console.log("my pen name is",pen.name)
+console.log(` my pen is $(pen.name)`)
+
+let name1="kiran"
+let name2="kiran"
+if(name1==name2)
+{
+    console.log("yes")
+}
+else{
+    console.log("no")
+}
+
+name1==name2 ? console.log("yes"):console.log("no")
+alert("hello welcome");
